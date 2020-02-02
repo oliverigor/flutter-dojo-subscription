31fc146a87a0ee3291e4d78f8b517525a554c08d
Igor Martins <m.oliverunb@gmail.com>
2020-02-01 22:02:53 -0500

    Route navigation and thats it

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,8 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
-  var _name = [];
+  var _name = '';
+  var _email = '';
 
   void _incrementCounter() {
     setState(() {
@@ -64,9 +65,8 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _name.add(myNameController.text);
-      myPasswordController.text = '';
-      myNameController.text = '';
+      _name = myNameController.text;
+      _email = myPasswordController.text;
     });
   }
 
@@ -79,23 +79,18 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: SafeArea(
           child: Column(children: <Widget>[
         Container(
-            margin: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 40),
+            margin: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 100),
             child: CircleAvatar(
               radius: 100.0,
               backgroundImage: NetworkImage(
                   'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
             )),
         Container(
-            height: 200.0,
-            margin: const EdgeInsets.symmetric(horizontal: 6.0, vertical: 40),
+            height: 150.0,
+            margin: const EdgeInsets.symmetric(horizontal: 6.0),
             child: ListView(
               children: <Widget>[
                 ListTile(
@@ -109,28 +104,74 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 ListTile(
-                  leading: Icon(Icons.lock),
+                  leading: Icon(Icons.email),
                   title: TextField(
-                    obscureText: true,
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
-                      labelText: 'Password',
+                      labelText: 'Email',
                     ),
                     controller: myPasswordController,
                   ),
                 ),
-                Column(
-                    children: _name
-                        .map((item) => new Row(children: <Widget>[Text(item)]))
-                        .toList())
               ],
-            ))
+            )),
+        Container(
+            width: 100,
+            child: RaisedButton(
+              color: Colors.lightBlue,
+              child: Text(
+                'Join in!',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                _incrementCounter();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => FirstRoute(),
+                      settings: RouteSettings(
+                          arguments: ScreenArguments(_name, _email))),
+                );
+              },
+            )),
       ])),
+    );
+  }
+}
+
+class ScreenArguments {
+  final String name;
+  final String email;
+
+  ScreenArguments(this.name, this.email);
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final ScreenArguments info = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                  image: NetworkImage(
+                      'https://media3.giphy.com/media/aOften89vRbG/giphy.gif?cid=790b761191dcb1e98801cd65b752f66f9e8a99334de7bf39&rid=giphy.gif')),
+              Text('Hey ' + info.name),
+              Text('And email was sent to: ' + info.email)
+            ]),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          Navigator.pop(
+            context,
+          );
+        },
         tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.arrow_left),
+      ),
     );
   }
 }
